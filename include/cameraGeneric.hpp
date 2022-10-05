00416529b51384df9cde9615071339ab198161a1
Vittorio Francescon <vittorio.francescon@gmail.com>
2022-10-05 11:40:57 +0100

    Set up computer vision framework

---
+++ include/cameraGeneric.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <eigen3/Eigen/Core>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <pylon/PylonIncludes.h>
+
+int main(int argc, char* argv[]);
+
