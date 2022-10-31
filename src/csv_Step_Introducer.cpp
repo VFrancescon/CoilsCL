9167467f0621724c7ce5c3a870f2f27be37de6b4
Vittorio Francescon <vittorio.francescon@gmail.com>
2022-10-31 19:36:42 +0000

    Wrote large strokes of demo from damith plan

--- src/csv_Step_Introducer.cpp
+++ src/csv_Step_Introducer.cpp
@@ -7,66 +7,37 @@ std::vector<std::string> row;
 std::vector<float> bx, by, bz;
 
 int main(int argc, char* argv[]){
-    std::string filename = "/home/vittorio/coil_manipulator/Uopt2_Nav2_OPT2RE.csv";
-    if(argc == 2) {
-        filename = argv[1];
-        }
-    
-    std::ifstream file(filename, std::ios::in);
-    std::string line, word;
-    
-    getline(file,line);
 
-    while(std::getline(file, line)){
-        int counter = 0;
-        std::stringstream sstr(line);
-        // std::cout << line << "\n";
-        while(std::getline(sstr, word, ',')){
-
-            if(counter == 6) {
-                bx.push_back(std::stof(word)* -3);
-                bx.push_back(std::stof(word)* -3);
-                bx.push_back(std::stof(word)* -3);
-            }
-            if(counter == 7) {
-                by.push_back(std::stof(word) * -4);
-                by.push_back(std::stof(word) * -4);
-                by.push_back(std::stof(word) * -4);}
-            
-            if(counter == 8) {
-                bz.push_back(std::stof(word) * -3);
-                bz.push_back(std::stof(word) * -3);
-                bz.push_back(std::stof(word) * -3);
-            }
-            counter++;
-        }
-    }
-    std::cout << "Size of input: " << bx.size() << "\n";
-    
-    std::vector<float> bxINV = bx, byINV = by, bzINV = bz;
-    std::reverse(bxINV.begin(), bxINV.end());
-    std::reverse(byINV.begin(), byINV.end());
-    std::reverse(bzINV.begin(), bzINV.end());
-
-
-    // for(int i = 0; i < bx.size(); i++){
-    //     std::cout << "Bx: " << bx[i] << " By: " << by[i] << " Bz: " << bz[i] <<"\n";    
-    // }
-
-    // MiddlewareLayer mid("/dev/ttyUSB2","/dev/ttyUSB3", "/dev/ttyUSB1", "/dev/ttyUSB4", "/dev/ttyUSB0");
     MiddlewareLayer mid;
-
-    // std::cout << "Everything initialised properly.\nWe will write current to all PSUs starting at 0.01, 20 steps.\nPress enter to begin";
-    std::cout << "Press enter to begin stepping through the csv";
+    std::cout << "Everything initialised properly. Press enter to begin.";
     std::cin.get();
-    
-    // std::thread th1(&MiddlewareLayer::set3DVector, &mid, bx, by, bz);
-    
-    std::cout << "Inserting\n";
-    mid.set3DVectorIN(bx, by, bz);
-    std::cout << "Retracting\n";
-    mid.set3DVectorOUT(bxINV, byINV, bzINV);
-    
+    //1. bile duct
 
-    //std::cout << "Field in X direction at the end of writing loop: " << mid.getXField();
+    //Apply field
+    mid.set3DField(-25, 0, -5);
+
+    //step 50 times (to the limit)
+    mid.stepIntroducer(50);
+
+    //undo field and retract
+    mid.set3DField(0,0,0);
+    mid.retractIntroducer(50);
+
+    //2. straight channnel
+    
+    //apply +bx
+    mid.set3DField(25, 0, -5);
+    mid.stepIntroducer(20);
+    //apply -bx
+    mid.set3DField(-25,0,-5);
+    mid.stepIntroducer(20);
+    //apply b=0
+    mid.set3DField(0,0,0);
+    mid.stepIntroducer(20);
+
+    //3. Wrap up
+    mid.retractIntroducer(60);
+
+    return 0;
+
 }
