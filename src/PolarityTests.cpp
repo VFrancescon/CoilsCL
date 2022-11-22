419e31972a828e191842e551690b86cac142350c
Vittorio <vittorio.francescon@gmail.com>
2022-11-22 11:52:55 +0000

    Added some extra bells to polarity tests

--- src/PolarityTests.cpp
+++ src/PolarityTests.cpp
@@ -35,41 +35,48 @@ int main(int argc, char* argv[]){
     }
 
 
-    DXKDP_PSU psu1(addrOne);
-    DXKDP_PSU psu2(addrTwo);
-    Teslameter tmeter(addrTM);
+    DXKDP_PSU psu1(addrOne, 0.01, 0.01);
+    DXKDP_PSU psu2(addrTwo, 0.01, 0.01);
+    // Teslameter tmeter(addrTM);
 
 
     std::cout << "Ready to go. Press enter";
     std::cin.get();
+    psu1.PoCtrl(0x01);
+    psu2.PoCtrl(0x01);
+    psu1.WriteVI(20,10);
+    psu2.WriteVI(20,10);
 
     //Step 1: 0 0
     std::cout << "Step 1: 0 0";
     psu1.setPolarity(0x00);
     psu2.setPolarity(0x00);
-    std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
-
+    // std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
+    std::cin.get();
 
     std::cout << "\n\n--------------------------------------\n\n";
     //Step 2: 0 1
     std::cout << "Step 1: 0 1";
     psu1.setPolarity(0x00);
     psu2.setPolarity(0x01);
-    std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
+    // std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
+    std::cin.get();
 
     std::cout << "\n\n--------------------------------------\n\n";
     //Step 2: 1 1
     std::cout << "Step 1: 1 1";
     psu1.setPolarity(0x01);
     psu2.setPolarity(0x01);
-    std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
+    // std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
+    std::cin.get();
 
     std::cout << "\n\n--------------------------------------\n\n";
     //Step 2: 1 0
     std::cout << "Step 1: 1 0";
     psu1.setPolarity(0x01);
     psu2.setPolarity(0x00);
-    std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
+    // std::cout << " read field " << tmeter.SingleAxisReading(axisToRead) << "\n";
+    std::cin.get();
 
 
     return 0;
